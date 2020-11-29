749a674302dcbdac26198e5368bb00fdfc77d16a
Miradel <miradel123@Gmail.com>
2020-11-29 11:59:40 -0500

    Added templates in the resources

---
+++ src/main/java/com/meradel/MeradelApplication.java
@@ -0,0 +1,13 @@
+package com.meradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeradelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeradelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meradel/MeradelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meradel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeradelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
